7f3e132a2e7552912612306f8ac95db6e817a7ee
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-11 11:10:48 -0500

    Configurar CSRF para Railway

--- emaus/settings.py
+++ emaus/settings.py
@@ -28,6 +28,10 @@ DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://ministeriodemusicaemaus.up.railway.app',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
